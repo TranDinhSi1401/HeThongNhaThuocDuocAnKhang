855af6192974a3f90425ec1300e2c8718ce12772
Tran Dinh Si <trandinhsi1401@gmail.com>
2025-10-16 17:50:37 +0700

    Hoàn thành 70% giao diện chính

---
+++ src/hethongnhathuocduocankhang/HeThongNhaThuocDuocAnKhang.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hethongnhathuocduocankhang;
+
+import hethongnhathuocduocankhang.gui.GiaoDienChinhGUI;
+
+/**
+ *
+ * @author trand
+ */
+public class HeThongNhaThuocDuocAnKhang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+            new GiaoDienChinhGUI().setVisible(true);
+        });
+    }
+    
+}
