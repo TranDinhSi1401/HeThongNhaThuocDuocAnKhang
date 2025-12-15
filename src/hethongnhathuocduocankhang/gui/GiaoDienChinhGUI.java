394c554bf74a0e9601f0c9c2f2a1002aad582b2a
Khang <khangdz013@gmail.com>
2025-12-15 14:25:05 +0700

    Done quản lý phiếu nhập

--- src/hethongnhathuocduocankhang/gui/GiaoDienChinhGUI.java
+++ src/hethongnhathuocduocankhang/gui/GiaoDienChinhGUI.java
@@ -68,6 +68,8 @@ public class GiaoDienChinhGUI extends javax.swing.JFrame {
                             showPanel(new QuanLiLichSuCaLamGUI());
                         case 9 ->
                             showPanel(new QuanLiPhieuTraHangGUI());
+                        case 10 ->
+                            showPanel(new QuanLiPhieuNhapHangGUI());
                         default -> {
                         }
                     }
